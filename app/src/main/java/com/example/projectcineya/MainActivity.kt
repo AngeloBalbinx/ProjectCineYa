00890f8e3a7fbe4661bc9185ca5d18511a44c4b6
Angelo Balbin <balbinangelo94@gmail.com>
2023-06-17 20:49:48 -0500

    Diseño del Navigation drawer

--- app/src/main/java/com/example/projectcineya/MainActivity.kt
+++ app/src/main/java/com/example/projectcineya/MainActivity.kt
@@ -3,20 +3,31 @@ package com.example.projectcineya
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.projectcineya.databinding.ActivityMainBinding
+import com.example.projectcineya.databinding.ActivityMapsBinding
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.database.*
 import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding:ActivityMainBinding
     private lateinit var dataList: ArrayList<PeliculaClass>
     private lateinit var adapter: PeliculaAdapter
     var databaseReference:DatabaseReference?=null
     var eventListener:ValueEventListener?=null
+    private lateinit var drawerLayout:DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -70,6 +81,42 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+        drawerLayout=findViewById<DrawerLayout>(R.id.drawerLayout)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle=ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.abrirnav,R.string.cerrarnav)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_cines -> {
+                val intent = Intent(this, MapsActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.nav_inicio -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+
+            R.id.nav_salir-> Toast.makeText(this, "Saliste de la sesión!", Toast.LENGTH_SHORT).show()
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            onBackPressedDispatcher.onBackPressed()
+        }
     }
     fun buscarLista(text:String){
         val buscarLista = ArrayList<PeliculaClass>()
@@ -79,6 +126,5 @@ class MainActivity : AppCompatActivity() {
             }
         }
         adapter.searchDataList(buscarLista)
-
     }
 }
